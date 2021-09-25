df368547e08610e64aca0a120a4096f8fce1740b
shruthishruu05 <shruthi.m@perfios.com>
2021-09-25 18:32:20 +0530

    ADDED : class that implements interface and add method

--- lib/src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ lib/src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -8,7 +8,8 @@ import java.util.*;
 import java.util.function.Predicate;
 
 
-public class AddressBook implements AddressBookIF {
+public class AddressBook implements AddressBookIF 
+{
 	public static String CSV_FILE_NAME = "AddressBook-file.csv";
 	public static String TXT_FILE_NAME = "AddressBook-file.txt";
 	public static String JSON_FILE_NAME = "AddressBook-file.json";
