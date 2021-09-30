8481b6e02b3e618a2fd91d9f461ca2e0f6211fe8
shruthishruu05 <shruthi.m@perfios.com>
2021-09-30 19:02:36 +0530

    REFCATORED : added new constructor

--- lib/src/main/java/com/bridgelabz/addressbook/contacts.java
+++ lib/src/main/java/com/bridgelabz/addressbook/contacts.java
@@ -1,19 +1,23 @@
 package com.bridgelabz.addressbook;
 
+import java.util.List;
+
 public class contacts {
 	private int contactID;
 	private String firstName;
 	private String lastName;
 	private String phoneNumber;
 	private String email;
+	private String added;
 	
-	public contacts(int contactID, String firstName, String lastName, String phoneNumber, String email) {
+	public contacts(int contactID, String firstName, String lastName, String phoneNumber, String email,String added) {
 		super();
 		this.contactID = contactID;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.phoneNumber = phoneNumber;
 		this.email = email;
+		this.added = added;
 	}
 	public contacts() {
 		
@@ -24,6 +28,13 @@ public class contacts {
 		this.lastName = lastName;
 		this.phoneNumber = phoneNumber;
 		this.email = email;
+	}
+	
+	public String getAdded() {
+		return added;
+	}
+	public void setAdded(String added) {
+		this.added = added;
 	}
 	public int getContactID() {
 		return contactID;
